ba56862b7509ec009972b94fcdfc8f323097cb9c
Maksim Andrianov <maksimandrianov1@yandex.ru>
2018-08-05 05:25:07 +0300

    [oop][patterns] Added singleton

---
+++ oop/patterns/singleton.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class Singleton {
+public:
+        Singleton(const Singleton &) = delete;
+        Singleton &operator=(const Singleton &) = delete;
+
+        static Singleton &Inctance() {
+                static Singleton inst;
+                return inst;
+        }
+
+        void print() {
+                std::cout << "print" << std::endl;
+        }
+
+private:
+        Singleton() {}
+        ~Singleton() {}
+};
+
+int main() {
+        Singleton::Inctance().print();
+}
