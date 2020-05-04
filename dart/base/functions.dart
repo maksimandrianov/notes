6bd6e76102213882c6edea5709934201d9a6db0a
Maksim Andrianov <maksimandrianov1@gmail.com>
2020-05-04 06:24:54 +0300

    Updated dart notes.

--- dart/base/functions.dart
+++ dart/base/functions.dart
@@ -1,5 +1,23 @@
 int sum(int a, int b) => a + b;
 
+void funcWithOptPos(int a, [int b, int c=1]) {
+  print("funcWithOpt($a $b $c)");
+}
+
+void funcWithOptNamed(int a, {int b:1, int c}) {
+  print("funcWithOpt($a $b $c)");
+}
+
+int funcWithFunc1(int sum(int a, int b), int a , int b) {
+  return sum(a, b);
+}
+
+typedef int SumFunc(int a, int b);
+
+int funcWithFunc2(SumFunc sum, int a , int b) {
+  return sum(a, b);
+}
+
 void main() {
   print(sum(4, 7));
 
@@ -12,4 +30,18 @@ void main() {
   };
 
   print(mult(3, 4));
+
+  funcWithOptPos(1);
+  funcWithOptPos(1, 2);
+  funcWithOptPos(1, 2, 3);
+
+  funcWithOptNamed(1);
+  funcWithOptNamed(1, b:2);
+  funcWithOptNamed(1, b:2, c:3);
+
+  print(varSum is Object);
+  print(varSum is Function);
+
+  print("${funcWithFunc1(sum, 4, 8)}");
+  print("${funcWithFunc2(sum, 4, 8)}");
 }
